970ff4385120c7abae51d1ef5e9a3876f95dacf9
onodera-g <ga19981023ku@gmail.com>
2023-06-10 11:34:41 +0000

    Chapter 6

--- F6X4.f
+++ F6X4.f
@@ -4,18 +4,18 @@
       PROGRAM F6X4
             INTEGER I,J,FREQ(0:9)
             REAL DATA(30)
-*
+*FREQの初期化
             DATA FREQ/10*0/
-*
+*DATAの入力
             READ(5,10)(DATA(I),I=1,30)
 10          FORMAT(6F5.0)
-*
+*度数の振り分け
             DO 20 J=1,30
                   I=INT(DATA(J)/10)
                   WRITE(6,*)DATA(J),I,FREQ(I)
                   FREQ(I)=FREQ(I)+1
 20          CONTINUE
-*
+*結果の表示
             WRITE(6,30)
 30          FORMAT(' CLASS   RANGE   FREQUENCY')
             WRITE(6,40)
